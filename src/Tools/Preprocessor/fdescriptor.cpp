3bda29bda063c5df5fe1639ff3cd2331d2341975
VSEGA <tupjnin@gmail.com>
2020-11-20 10:54:39 +0700

    Changes

--- src/Tools/Preprocessor/fdescriptor.cpp
+++ src/Tools/Preprocessor/fdescriptor.cpp
@@ -1,15 +1,10 @@
 #include "fdescriptor.h"
 #include "comments.h" // comments()
 
-void preprocessor(std::string &buffer, std::string str) {
-    buffer = "";
-    FILE *in = fopen(str, "r"); // Input File
+void preprocessor(std::string str) {
     SettingsBuffer *options; // Settings Buffer
-    char buffer[2]; // char buffer[0 is char, 1 is next char]
-
-    buffer[0] = fgetc(in);
-
-    while (buffer[1] != EOF) {
+    ++str.begin();
+    for(char i: str) {
         buffer[0] = buffer[1];
         buffer[1] = fgetc(in);
         fputc(comments(buffer, options), out);
